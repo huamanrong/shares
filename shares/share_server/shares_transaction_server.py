106a0570753830c025876d396b5c8713367e8798
huamanrong <1033674932@qq.com>
2021-02-10 16:11:40 +0800

    更改mysql until

--- shares/share_server/shares_transaction_server.py
+++ shares/share_server/shares_transaction_server.py
@@ -1,20 +1,74 @@
+import time
 from shares.share_until.mysql_until import *
 from shares.share_server import database
+from shares.share_until.shares_calculation import *
 
 
 class SharesTransactionServer:
-    def __init__(self, logger, shares_user, shares_name, cost, amount, date_time):
+    def __init__(self, logger, shares_user, shares_name, cost, amount, reasons, date_time):
         self.logger = logger
         self.shares_user = shares_user
         self.shares_name = shares_name
         self.cost = cost
         self.amount = amount
+        self.reasons = reasons
         self.date_time = date_time
 
+    @staticmethod
+    def is_float(str_float):
+        # 判断字符串是否是小数
+        str_list = str_float.split('.')
+        if len(str_list) > 2 or [i for i in filter(lambda x: not x.isdigit(), str_list)]:
+            return False
+        else:
+            return True
+
+    @staticmethod
+    def is_str_time(str_time):
+        try:
+            time.strptime(str_time, '%Y-%m-%d %H:%M:%S')
+            return True
+        except:
+            return False
+
     def shares_buy(self):
-        sql = "select id, cost, amount from buy_already where name=%s and status=1;"
-        result = execute_select_sql(database.conf, sql, self.logger, self.shares_name)
+        sql = "select id, cost, amount, symbol from buy_already where name=%s and status=1 and user_id=%s;"
+        result = execute_select_sql(database.conf, sql, self.logger, self.shares_name, self.shares_user)
         if result:
-            pass
+            if not(self.cost and self.amount and self.date_time):
+                return 1, '输入不全'
+            elif not(self.is_float(self.cost) and self.amount.isdigit() and self.is_str_time(self.date_time)):
+                return 1, '价格或数量或时间输入错误'
+            else:
+                cnn = get_connect(database.conf)  # 获取数据库连接
+                try:
+                    after_cost = price_calculation(result[0][1], result[0][2], 0, float(self.cost), int(self.amount))
+                    after_amount = result[0][2]+int(self.amount)
+                    update_buy_already = 'update buy_already set cost=%s,amount=%s where id=%s;'
+                    execute_change_sql(cnn, update_buy_already, self.logger, after_cost, after_amount, result[0][0])
+                    select_surplus_money = 'select surplus_money from shares_user where id=%s;'
+                    user_surplus_money = execute_select_sql(database.conf, select_surplus_money, self.logger, self.shares_user)[0][0]
+                    user_surplus_money = user_surplus_money - float(self.cost)*int(self.amount)
+                    update_surplus_money = 'update shares_user set surplus_money=%s where id=%s'
+                    execute_change_sql(cnn, update_surplus_money, self.logger, user_surplus_money, self.shares_user)
+                    insert_transaction_records = '''
+                    INSERT INTO transaction_records ( user_id, hold_share_id, type, `name`, symbol, cost, amount, reasons, transaction_date ) 
+                    VALUE ( %s, %s, 0, %s, %s, %s, %s, %s, %s )
+                    '''
+                    execute_change_sql(cnn, insert_transaction_records, self.logger, user_surplus_money, self.shares_user)
+
+
+                    commit_close(cnn)    # 执行数据库并关闭连接
+                except Exception as e:
+                    self.logger.exception('执行更新数据库操作失败')
+                    mysql_rollback(cnn)
+                    return 1, '执行更新数据库操作失败'
         else:
             pass
+
+
+if __name__ == '__main__':
+    from shares.share_until.standard_logging import my_log
+    logger = my_log()
+    transaction = SharesTransactionServer(logger, 1, '杉杉股份', '16', '200', '2021-2-10 10:45:00')
+    transaction.shares_buy()
--- shares/share_until/mysql_until.py
+++ shares/share_until/mysql_until.py
@@ -1,6 +1,12 @@
 import pymysql
 
 
+def get_connect(conf):
+    cnn = pymysql.connect(**conf)
+    cursor = cnn.cursor()
+    return cnn, cursor
+
+
 def execute_select_sql(conf, sql, logger, *args):
     cnn = pymysql.connect(**conf)
     cursor = cnn.cursor()
@@ -15,21 +21,28 @@ def execute_select_sql(conf, sql, logger, *args):
         cnn.close()
 
 
-def execute_change_sql(conf, sql, logger, *args):
-    cnn = pymysql.connect(**conf)
-    cursor = cnn.cursor()
+def execute_change_sql(con, sql, logger, *args):
+    cnn, cursor = con
     try:
-        # sql = "select * from user where username=%s and password=%s"
-        # cursor.execute(sql,user,pwd) #execute会帮我们做字符串拼接，推荐这种写法
         cursor.execute(sql, args)
         insert_id = None
-        if sql.strip().lower().startswith() == 'insert':
+        if sql.strip().lower().startswith('insert'):
             insert_id = cnn.insert_id()     # 返回插入数据的主键id
-        cnn.commit()
         return insert_id
     except Exception as e:
         logger.exception('执行出错\n%s' % e)
         raise e
-    finally:
-        cursor.close()
-        cnn.close()
+
+
+def commit_close(con):
+    cnn, cursor = con
+    cnn.commit()
+    cursor.close()
+    cnn.close()
+
+
+def mysql_rollback(con):
+    cnn, cursor = con
+    cnn.rollback()
+    cursor.close()
+    cnn.close()
